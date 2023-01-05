2005a969a165f649d0b838acfc507174c404c26f
Sami Lechner <sami.lechner@sap.com>
2023-01-05 16:47:24 +0100

    new PopUp test

---
+++ popUp/popUp/ButtonPopup.cs
@@ -0,0 +1,33 @@
+﻿using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+
+namespace popUp
+{
+    public partial class ButtonPopup : Popup
+    {
+        protected Button CancelButton = null;
+
+        public ButtonPopup()
+        {
+            StackLayout contentLayout = new StackLayout()
+            {
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            CancelButton = new Button()
+            {
+                //FontFamily = EltepiGlobalUI.FONT_MAIN_FONT,
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                HeightRequest = Device.GetNamedSize(NamedSize.Large, typeof(Label)) * 3,
+                BorderWidth = 0,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                BackgroundColor = Color.Chocolate,
+            };
+            CancelButton.Text = "exit";
+            CancelButton.Command = new Command(() => Dismiss(null));
+            contentLayout.Children.Add(CancelButton);
+            Content = contentLayout;
+        }
+
+
+    }
+}
---
+++ popUp/popUp/MainPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.Forms;
+
+
+namespace popUp
+
+{
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            StackLayout contentView = new StackLayout()
+            {
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            Button showList = new Button()
+            {
+                Text = "click",
+                BackgroundColor = Color.DarkGreen
+            };
+            showList.Clicked += ShowList_ClickedAsync;
+            contentView.Children.Add(showList);
+            Content = contentView;
+        }
+
+        private async void ShowList_ClickedAsync(object sender, EventArgs e)
+        {
+            var result = await Navigation.ShowPopupAsync(new ButtonPopup());
+        }
+    }
+}
